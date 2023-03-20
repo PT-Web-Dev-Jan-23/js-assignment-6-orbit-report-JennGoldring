0a07e14522c788a3ce95beca8165fa35ebce25d9
Jennifer <125608864+JennGoldring@users.noreply.github.com>
2023-03-19 21:05:30 -0500

    added satellite count

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,17 +15,20 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
-  countByType(type: string): number {
-	let count = 0;
-	if (this.satellites) {
-	  for (let i = 0; i < this.satellites.length; i++) {
-		 if (this.satellites[i].type === type) {
-			count++;
-		 }
-	  }
-	}
-	return count;
- }
-
+  countByType(type: string): { count: number, total: number } {
+    let count = 0;
+    let total = 0;
+    if (this.satellites) {
+      for (let i = 0; i < this.satellites.length; i++) {
+        if (this.satellites[i].type === type) {
+          count++;
+        }
+        total++;
+      }
+    }
+	console.log(`Count of ${type} satellites: ${count}`);
+	console.log(`Total satellites: ${total}`);
+	return { count, total };
+  }
 
 }
